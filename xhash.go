820b6af8a6baa9c0460e0370d26e308b23b1aa5c
Ricardo Branco <rbranco@suse.de>
2019-09-03 15:40:23 +0200

    Use parallel exclusively

--- xhash.go
+++ xhash.go
@@ -1,12 +1,39 @@
 package main
 
 import (
+	"crypto/md5"
 	"errors"
+	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"sort"
 	"sync"
 )
 
+// A result is the product of reading and summing a file using MD5.
+type result struct {
+	path string
+	sum  []byte
+	err  error
+}
+
+func sumFile(path string) ([]byte, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return nil, err
+	}
+
+	return hash.Sum(nil), nil
+}
+
+// sumFiles starts goroutines to walk the directory tree at root and digest each
 // regular file.  These goroutines send the results of the digests on the result
 // channel and send the result of the walk on the error channel.  If done is
 // closed, sumFiles abandons its work.
@@ -57,7 +84,7 @@ func sumFiles(done <-chan struct{}, root string) (<-chan result, <-chan error) {
 // from file path to the MD5 sum of the file's contents.  If the directory walk
 // fails or any read operation fails, MD5All returns an error.  In that case,
 // MD5All does not wait for inflight read operations to complete.
-func MD5All_parallel(root string) (map[string][]byte, error) {
+func MD5All(root string) (map[string][]byte, error) {
 	// MD5All closes the done channel when it returns; it may do so before
 	// receiving all the values from c and errc.
 	done := make(chan struct{})
@@ -76,4 +103,32 @@ func MD5All_parallel(root string) (map[string][]byte, error) {
 		return nil, err
 	}
 	return m, nil
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage: %s DIRECTORY\n", os.Args[0])
+	os.Exit(1)
+}
+
+func main() {
+	// Calculate the MD5 sum of all files under the specified directory,
+	// then print the results sorted by path name.
+
+	if len(os.Args) != 2 {
+		usage()
+	}
+
+	m, err := MD5All(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var paths []string
+	for path := range m {
+		paths = append(paths, path)
+	}
+	sort.Strings(paths)
+	for _, path := range paths {
+		fmt.Printf("%x  %s\n", m[path], path)
+	}
 }
